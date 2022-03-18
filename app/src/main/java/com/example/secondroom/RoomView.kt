49de802918545229a864aef513d50ad55bab522d
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2022-03-18 20:03:29 +0500

    Back Button Complete

--- app/src/main/java/com/example/secondroom/RoomView.kt
+++ app/src/main/java/com/example/secondroom/RoomView.kt
@@ -1,11 +1,35 @@
 package com.example.secondroom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
+
 
 class RoomView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_view)
+
+        var actionBar = supportActionBar
+
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
